88821f62eb769e8a5c0c0fe751378812bb8d26a6
jannenold <73650127+jannenold@users.noreply.github.com>
2020-12-04 13:53:06 +0100

    Update mps_extract.py

--- mps_extract.py
+++ mps_extract.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 '''
 Author: Janne Nold 
@@ -25,14 +23,15 @@ import warnings
 import pandas as pd 
 
 
-def mps_extract(filename, sr = 44100, n_fft, hop_length, mps_n_fft, 
-                mps_hop_length = mps_n_fft, plot_mps = True, **kwargs)
+def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft = 500, 
+                mps_hop_length = 500, plot_mps = False, **kwargs)
 
                
 '''                
 Input
 
 filename:       str, path to wav files to be converted
+output path:    ?
 sr:             int, sampling rate for wav file (Default: 44100 Hz)
 n_fft:          int, window size for mel spectrogram extraction
 hop_length:     int, step size for mel spectrogram extraction
@@ -43,13 +42,11 @@ kwargs:         additional keyword arguments that will be transferred to librosa
 
 Output
 
-tuple of a feature representation (2-dimensional array: time x feature)
+tuple of a feature representation (2-dimensional array: samples x feature)
 repitition time in seconds
 names of all features (list of strings of mod/s for each mod/Hz)        
 
 '''
-
-
 
 # Extracting wav files from directory
 wav, _ = lbr.load(filename, sr=sr) 
@@ -58,22 +55,24 @@ wav, _ = lbr.load(filename, sr=sr)
 # Extracting Mel spectrogram
 mel_spec = lbr.feature.melspectrogram(y=wav, sr=sr, hop_length=hop_length,
                                               **kwargs)
+mel_spec = mel_spec.T
 
 # Checking Input parameters
-if mps_n_fft >= mel_spec.shape[1]
+if mps_n_fft >= mel_spec.shape[0]:
     raise ValueError("The mps window size exceeds the Mel spectrogram. Please enter a smaller integer.")
 
-if mps_hop_length >= mel_spec.shape[1]
+if mps_hop_length >= mel_spec.shape[0]:
     raise ValueError("The mps step size exceeds the Mel spectrogram. Please enter a smaller integer.")
 
 
 # Extracting mps
 mps_all = []
-nyquist_mps = np.ceil(mel_spec.shape[0]/2)
+nyquist_mps = np.ceil(mel_spec.shape[1]/2)
 
 
-for i in range(1,101)
-    mps = np.fft.fft2(mel_spec[:,mps_n_fft*(i-1):mps_n_fft*i])
+
+for i in range(1,101):
+    mps = np.fft.fft2(mel_spec[mps_n_fft*(i-1):mps_n_fft*i,:])
     
     mps = np.abs(np.fft.fftshift(mps))
     
@@ -86,35 +85,47 @@ for i in range(1,101)
    
     # Concatinating the MPS row-wise
     mps_all = np.concatenate(mps_all)
-                     
-                     
+    
+
+# Raw Signal
+sr = 44100
+raw_length_sec = (len(wav)/sr)
+raw_length_min = raw_length_sec/60
+
+#number_samples = sr*len(wav)/1000 ?
+
+# Sampling Rate in Mel Spectrogram
+fs_spectrogram = round(len(mel_spec)/(raw_length_sec))#if i roiund it the fs_spec will be 0 
+
+# Sampling rate in MPS 
+fs_mps = round(mps_n_fft/(raw_length_min))
+
+ 
 # Extract Axis units for plotting 
-
-# Calculate axes step sizes for MEL spectrogram
-sinal_sec = n_fft/sr
-mel_Hz_s = sr/n_fft # step size for MEL spectrogram Hz/sample (fundamental frequency)
-mel_time_s = hop_length/sr # step size for MEL spectrogram sec/sample 
-
 # Calculate step sizes for MPS
+freq_step_log = np.log(mel_spec[1,:])
+freq_step_log = freq_step_log[1] - freq_step_log[0]
 
+time_step_log = mel_spec[:,1]
+time_step_log = time_step_log[1] - time_step_log[0]
 
 # Calculate labels for X and Y axes
-mps_freqs = np.fft.fftshift((np.fft.fftfreq(mel_spec.shape[1], d = freq_step_log))
-mps_times = np.fft.fftshift((np.fft.fftfreq(mel_spec.shape[0], d = time_step_log))                      
-                            
-# Plotting the MPS
-if plot_mps = True
-  fig, ax = plt.subplots()
-  ax.imshow(np.log(mps_all))
-  ax.pcolormesh(mps_times, mps_freqs, plot_mps, cmap ='viridis')
-  ax.contour(mps_times, mps_freqs, mps_plt,np.percentile(plot_mps,[80,90,95,99]))       
-  ax.set_title('Modulation Power Spectrum')
-  ax.set_xlabel('mod/s')
-  ax.set_ylabel('cyc/oct')
-                     
+mps_freqs = np.fft.fftshift(np.fft.fftfreq(mel_spec.shape[1], d = freq_step_log)) # returns fourier transformed freuqencies which are already shifted (lower freq in center))
+mps_times = np.fft.fftshift(np.fft.fftfreq(mel_spec.shape[0], d = time_step_log))
+
+
+if plot_mps = True:
+    fig, ax = plt.subplots()
+    plt.imshow(mps_all[0])
+    ax.pcolormesh(mps_times, mps_freqs, plot_mps, cmap ='viridis')
+    ax.contour(mps_times, mps_freqs, mps_plt,np.percentile(plot_mps,[80,90,95,99]))       
+    ax.set_title('Modulation Power Spectrum')
+    ax.set_xlabel('mod/s')
+    ax.set_ylabel('cyc/oct')
+    
+
 # Extracting feature names                     
-names_features = ['{0:.2f} mod/s {1:.2f} cyc/oct'.format(mps_time, mps_freq) \
-    for mps_time in mps_times for mps_freq in mps_freqs
+names_features = ['{0:.2f} mod/s {1:.2f} cyc/oct)'.format(mps_time, mps_freq) for mps_time in mps_times for mps_freq in mps_freqs]
                      
                      
 return mps_all, mps_rep_time, names_features
