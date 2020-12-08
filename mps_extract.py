d4970ae61243bc65702ba4bc9c3854d29717635b
jannenold <73650127+jannenold@users.noreply.github.com>
2020-12-08 09:43:27 +0100

    Update mps_extract.py

--- mps_extract.py
+++ mps_extract.py
@@ -32,11 +32,11 @@ Input
 
 filename:       str, path to wav files to be converted
 sr:             int, sampling rate for wav file (Default: 44100 Hz)
-n_fft:          int, window size for mel spectrogram extraction
-hop_length:     int, step size for mel spectrogram extraction
-mps_n_fft:      int, window size for mps extraction
-mps_hop_length: int, step size for mps extraction (Default: mps_hop_length = mps_n_fft)
-plot_mps:       bool, if true mps will be plotted (Default: True)
+n_fft:          int, window size for mel spectrogram extraction (Default: 512)
+hop_length:     int, step size for mel spectrogram extraction (Default: 512)
+mps_n_fft:      int, window size for mps extraction (Default: 500)
+mps_hop_length: int, step size for mps extraction (Default: 500)
+plot_mps:       bool, if true mps will be plotted (Default: False)
 kwargs:         additional keyword arguments that will be transferred to librosa's melspectrogram function
 
 Output
@@ -66,24 +66,36 @@ if mps_hop_length >= mel_spec.shape[0]:
 
 # Extracting mps
 mps_all = []
+mps_plot = []
 nyquist_mps = np.ceil(mel_spec.shape[1]/2)
 
 
 
 for i in range(1,101):
-    mps = np.fft.fft2(mel_spec[mps_n_fft*(i-1):mps_n_fft*i,:])
     
+    #Extract mps for predefined window
+    mps = np.fft.fft2(mel_spec[mps_n_fft*(i-1):mps_n_fft*i,:])
+   
+    # use absoulte and shifted frequencies
     mps = np.abs(np.fft.fftshift(mps))
-    mps_plot = np.abs(np.fft.fftshift(mps))
+    
+    # Define variable for later plotting
+    mps_plot.append(mps)
+   
     # Flattening the mps to a vector
     mps = np.reshape(mps,(1,np.size(mps)))
-   
-    mps_all = np.append(mps_all,mps)
     
-    mps_all = np.array(mps_all)
-   
-    # Concatinating the MPS row-wise
-    mps_all = np.concatenate(mps_all)
+    # Append mps to mps all
+    mps_all.append(mps)
+    
+# Convert mps_all into an array outside the loop
+mps_all = np.array(mps_all)
+
+# Convert mps_plot into an array outside loop
+mps_plot = np.array(mps_plot)
+
+# Concatinating the MPS row-wise
+mps_all = np.concatenate(mps_all)
     
 
 # Raw Signal
@@ -94,9 +106,11 @@ raw_length_min = raw_length_sec/60
 
 # Sampling Rate in Mel Spectrogram
 fs_spectrogram = round(len(mel_spec)/(raw_length_sec))
+# alternatively sr/hop_length
 
 # Sampling rate in MPS 
 fs_mps = round(mps_n_fft/(raw_length_min))
+# would we be able to do fs_spectrogram/mps_hop_length
 
  
 # Extract Axis units for plotting 
@@ -109,27 +123,28 @@ time_step_log = time_step_log[1] - time_step_log[0]
 
 # Calculate labels for X and Y axes
 mps_freqs = np.fft.fftshift(np.fft.fftfreq(mel_spec.shape[1], d = freq_step_log)) # returns fourier transformed freuqencies which are already shifted (lower freq in center))
-mps_times = np.fft.fftshift(np.fft.fftfreq(mps_n_fft, d = time_step_log/fs_spectrogram/fs_mps)) #????
+mps_times = np.fft.fftshift(np.fft.fftfreq(mps_n_fft, d = time_step_log/fs_spectrogram/fs_mps))
 
 
 if plot_mps = True:
-    fig, ax = plt.subplots()
-    image = ax.imshow(np.log(mps_all[0]))
-    ax.pcolormesh(mps_times, mps_freqs, image, cmap ='viridis')
-    ax.contour(mps_times, mps_freqs, image,np.percentile(image,[80,90,95,99]))       
-    ax.set_title('Modulation Power Spectrum')
-    ax.set_xlabel('mod/s')
-    ax.set_ylabel('cyc/oct')
-    plt.setp(ax, xlim = [-10,10], ylim=[0,9])
-
-    
-# Determine MPS repition time
-mps_rep_time = fs_spectrogram/mps_hop_length
+    fig, axs = plt.subplots(1, 2, figsize=(8, 4), sharex = True, sharey = True)
+    for ax, mps_plt in zip(axs, [np.log(mps_plot[0]),np.log(mps_plot).mean(0)]):
+        ax.pcolormesh(mps_times, mps_freqs, mps_plt, cmap ='viridis')
+        ax.contour(mps_times, mps_freqs, mps_plt, np.percentile(image,[80,90,95,99]))
+        _ = plt.setp(ax, xlim=[-10,10], ylim=[0,9])
+    axs[0].set_title('One Modulation Power Spectrum')
+    axs[1].set_title('Mean Modulation Power Spectrum')
+    axs[0].set_xlabel('Temporal Modulation mod/s')
+    axs[0].set_ylabel('Spectral Modulation cyc/oct')
     
 
 # Extracting feature names                     
 names_features = ['{0:.2f} mod/s {1:.2f} cyc/oct)'.format(mps_time, mps_freq) for mps_time in mps_times for mps_freq in mps_freqs]
-                     
+
+# Determine MPS repitition time 
+
+mps_rep_time = fs_spectrogram/mps_hop_length
+            
                      
 return mps_all, mps_rep_time, names_features
 
