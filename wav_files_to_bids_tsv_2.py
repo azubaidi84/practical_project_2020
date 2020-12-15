16579a823d64fddc5687f5641de3ae6818447fde
jannenold <73650127+jannenold@users.noreply.github.com>
2020-12-15 08:35:09 +0100

    Update wav_files_to_bids_tsv_2.py

--- wav_files_to_bids_tsv_2.py
+++ wav_files_to_bids_tsv_2.py
@@ -5,7 +5,7 @@ import json
 import os
 import librosa as lbr
 
-def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft = 500, mps_hop_length = 500, plot_mps = False, **kwargs):
+def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft = 500, mps_hop_length = 500, plot_mps = True, **kwargs):
     '''                
     Parameters
     ----------
@@ -16,7 +16,7 @@ def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft =
     hop_length:     int, step size for mel spectrogram extraction (Default: 512)
     mps_n_fft:      int, window size for mps extraction (Default: 500)
     mps_hop_length: int, step size for mps extraction (Default: 500)
-    plot_mps:       bool, if true the Mel spectrogram for the first window and according mps will be plotted (Default: False)
+    plot_mps:       bool, if true the Mel spectrogram for the first window and according mps will be plotted (Default: True)
     kwargs:         additional keyword arguments that will be transferred to librosa's melspectrogram function
     
     Returns
