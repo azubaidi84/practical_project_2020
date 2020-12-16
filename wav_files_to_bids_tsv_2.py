12adcd7b3566002c4c9479f3f4bffb884e424b73
jannenold <73650127+jannenold@users.noreply.github.com>
2020-12-16 14:01:00 +0100

    Update wav_files_to_bids_tsv_2.py

--- wav_files_to_bids_tsv_2.py
+++ wav_files_to_bids_tsv_2.py
@@ -5,18 +5,18 @@ import json
 import os
 import librosa as lbr
 
-def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft = 500, mps_hop_length = 500, plot_mps = True, **kwargs):
+def mps_extract(filename, sr = 44100, n_fft = 441, hop_length = 441, mps_n_fft = 500, mps_hop_length = 500, plot_mps = True, **kwargs):
     '''                
     Parameters
     ----------
     
     filename:       str, path to wav files to be converted
     sr:             int, sampling rate for wav file (Default: 44100 Hz)
-    n_fft:          int, window size for mel spectrogram extraction (Default: 512)
-    hop_length:     int, step size for mel spectrogram extraction (Default: 512)
+    n_fft:          int, window size for mel spectrogram extraction (Default: 441)
+    hop_length:     int, step size for mel spectrogram extraction (Default: 441)
     mps_n_fft:      int, window size for mps extraction (Default: 500)
     mps_hop_length: int, step size for mps extraction (Default: 500)
-    plot_mps:       bool, if true the Mel spectrogram for the first window and according mps will be plotted (Default: True)
+    plot_mps:       bool, if true the Mel spectrogram for the first window and according mps will be plotted (Default: False)
     kwargs:         additional keyword arguments that will be transferred to librosa's melspectrogram function
     
     Returns
@@ -116,8 +116,7 @@ def get_mel_spectrogram(filename, log=True, sr=44100, hop_length=512, **kwargs):
     a tuple consisting of the Melspectrogram of shape (time, mels), the repetition time in seconds, and the frequencies of the Mel filters in Hertz 
     '''
     wav, _ = lbr.load(filename, sr=sr)
-    melspecgrams = lbr.feature.melspectrogram(y=wav, sr=sr, hop_length=hop_length,
-                                              **kwargs)
+    melspecgrams = lbr.feature.melspectrogram(y=wav, sr=sr, hop_length=hop_length, **kwargs)
     if log:
         melspecgrams[np.isclose(melspecgrams, 0)] = np.finfo(melspecgrams.dtype).eps
         melspecgrams = np.log(melspecgrams)
@@ -154,7 +153,7 @@ if __name__ == '__main__':
     if len(args.start_time) > 1 and len(args.start_time) != len(args.file):
         raise ValueError('Number of files and number of start times are unequal. Start time has to be either one element or the same number as number of files.')
     for wav_file, start_time in zip(args.file, cycle(args.start_time)):
-        melspec, sr_spec, freqs = get_mel_spectrogram(wav_file, **config)
+        melspec, sr_spec, freqs = mps_extract(wav_file, **config)
         tsv_file = os.path.basename(wav_file).split('.')[0] + '.tsv.gz'
         json_file = os.path.basename(wav_file).split('.')[0] + '.json'
         if args.output:
